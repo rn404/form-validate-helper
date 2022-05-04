44aed50b2a960f977b61f4031071b7c2e8b620e3
rn404 <nekonekonekona@gmail.com>
2022-05-04 14:33:16 +0900

    update helper test

--- helper.test.ts
+++ helper.test.ts
@@ -1,36 +1,37 @@
 import { assertEquals } from './dependencies.ts';
 import { createFormValidateResult } from './helper.ts';
 
-Deno.test('Testing FormErrorClass #1', () => {
+Deno.test('createFormValidateResult: No error without errorMessages', () => {
   const parameters = {
-    name: 'hoge',
-    birth: {
-      year: 1964,
-      month: 5,
-      day: 24,
-    },
-    skill: ['ability1', 'ability2', 'ability3'],
+    name: 'tom',
+    email: 'aaa@aaa',
   } as const;
 
-  const { errors } = createFormValidateResult<typeof parameters, ['birthDay']>(
+  const { errors } = createFormValidateResult<typeof parameters>(
     parameters,
-    {
-      birthDay: 'something message',
-    },
+    {},
   );
 
-  assertEquals(errors?.invalid, {
-    name: false,
-    birth: false,
-    skill: [false, false, false],
-    birthDay: true,
-  });
+  assertEquals(errors, undefined);
 });
 
-Deno.test('Testing FormErrorClass #2', () => {
+Deno.test('createFormValidateResult: No error without errorMessages, even when array values are included', () => {
   const parameters = {
-    name: 'hoge',
-    email: undefined,
+    shipCodes: ['0001', '0002', '0003', '000004'],
+  } as const;
+
+  const { errors } = createFormValidateResult<typeof parameters>(
+    parameters,
+    {},
+  );
+
+  assertEquals(errors, undefined);
+});
+
+Deno.test('createFormValidateResult: errorMessages is correctly applied to invalid', () => {
+  const parameters = {
+    name: 'tom',
+    email: 'aaa@aaa',
   } as const;
 
   const { errors } = createFormValidateResult<typeof parameters>(
@@ -46,9 +47,50 @@ Deno.test('Testing FormErrorClass #2', () => {
   });
 });
 
-Deno.test('Testing FormErrorClass #3', () => {
+Deno.test('createFormValidateResult: errorMessages is correctly applied to invalid. Even if if parameter values are missing', () => {
+  const parameters: Partial<{
+    name: string;
+    email: string;
+  }> = {
+    name: 'tom',
+  };
+
+  const { errors } = createFormValidateResult<typeof parameters>(
+    parameters,
+    {
+      email: 'something message',
+    },
+  );
+
+  assertEquals(errors?.invalid, {
+    name: false,
+    email: true,
+  });
+});
+
+Deno.test('createFormValidateResult: Adding errorMessages with key that is not in parameters is applied to invalid', () => {
   const parameters = {
-    name: 'hoge',
+    name: 'tom',
+    email: 'aaa@aaa',
+  } as const;
+
+  const { errors } = createFormValidateResult<typeof parameters, ['other']>(
+    parameters,
+    {
+      other: 'something message',
+    },
+  );
+
+  assertEquals(errors?.invalid, {
+    name: false,
+    email: false,
+    other: true,
+  });
+});
+
+Deno.test('createFormValidateResult: Additional keys are not applied to invalid if there is no errorMessages', () => {
+  const parameters = {
+    name: 'tom',
     email: undefined,
   } as const;
 
@@ -65,7 +107,24 @@ Deno.test('Testing FormErrorClass #3', () => {
   });
 });
 
-Deno.test('Testing FormErrorClass #4', () => {
+Deno.test('createFormValidateResult: If errorMessages correctly refers to Array, invalid returns Array<boolean>', () => {
+  const parameters = {
+    shipCodes: ['0001', '0002', '0003', '000004'],
+  } as const;
+
+  const { errors } = createFormValidateResult<typeof parameters>(
+    parameters,
+    {
+      shipCodes: [undefined, undefined, undefined, 'too long'],
+    },
+  );
+
+  assertEquals(errors?.invalid, {
+    shipCodes: [false, false, false, true],
+  });
+});
+
+Deno.test('createFormValidateResult: If errorMessages correctly refers to Array, invalid returns Array<boolean>. Even if it contains empty', () => {
   const parameters = {
     shipCodes: ['0001', '0002', '0003', '000004'],
   } as const;
@@ -82,22 +141,9 @@ Deno.test('Testing FormErrorClass #4', () => {
   });
 });
 
-Deno.test('Testing FormErrorClass #5', () => {
+Deno.test('createFormValidateResult: errorMessages always override invalid, even when array values are included', () => {
   const parameters = {
-    shipCodes: ['0001', '0002', '0003', '000004'],
-  } as const;
-
-  const { errors } = createFormValidateResult<typeof parameters>(
-    parameters,
-    {},
-  );
-
-  assertEquals(errors?.invalid, undefined);
-});
-
-Deno.test('Testing FormErrorClass #6', () => {
-  const parameters = {
-    skills: []
+    skills: [],
   } as const;
 
   const { errors } = createFormValidateResult<typeof parameters>(
@@ -106,6 +152,30 @@ Deno.test('Testing FormErrorClass #6', () => {
   );
 
   assertEquals(errors?.invalid, {
-    skills: true
+    skills: true,
+  });
+});
+
+Deno.test('createFormValidateResult: errorMessages always override invalid, even when object values are included', () => {
+  const parameters = {
+    address: {
+      zipCode: '123-4567',
+      area: 'area01',
+      town: 'town01',
+      contact: {
+        telnumber: '810-9999-9999'
+      }
+    }
+  } as const;
+
+  const { errors } = createFormValidateResult<typeof parameters>(
+    parameters,
+    {
+      address: 'something wrong'
+    },
+  );
+
+  assertEquals(errors?.invalid, {
+    address: true,
   });
 });
